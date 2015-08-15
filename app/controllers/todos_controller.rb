2f61fa675d02e2ff4c7e1bcf25c64d3bd7026a4b
smurfik <smurfik@mlpinit.com>
2015-08-15 19:32:25 -0400

    Add create, update, delete actions

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,30 @@
 class TodosController < ApplicationController
+
+  def index
+    @todo = Todo.new
+    @todos = Todo.all
+  end
+
+  def create
+    @todo = Todo.new(permitted_params)
+    @todo.save
+    redirect_to root_path 
+  end
+
+  def permitted_params
+    params.require(:todo).permit(:id, :description, :status)
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+    @todo.update_attributes(permitted_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.delete
+    redirect_to root_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-    
+
+  resources :todos, except: :index    
   root 'todos#index'
 
   # Example of regular route:
